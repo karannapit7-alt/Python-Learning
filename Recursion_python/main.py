8eac8464c6875fd6534c6c256de149cbad83ba2e
KARAN THAKUR <karannapit7@gmail.com>
2026-01-16 20:38:18 +0530

    Python Recursion
    Recursion & Let's Pratices

---
+++ Recursion_python/main.py
@@ -0,0 +1,34 @@
+# print n to 1 backwards
+
+def show(n):
+    if(n == 0):
+        return
+    print(n)
+    show(n-1)
+
+show(6)
+
+
+
+# write a recursive function to calculate the sum of first n natural number.
+
+def cal_sum(n):
+    if(n == 0):
+        return 0
+    return cal_sum(n-1) + n
+
+sum = cal_sum(5)
+print(sum)
+
+
+
+def print_list(list,idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list,idx+1)
+
+Fruits = ["mango","Banana","watermelon","apple"]
+
+print_list(Fruits)
+    
